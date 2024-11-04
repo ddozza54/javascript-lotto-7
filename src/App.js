c74dc23dc4ad37a4f9c4e05d7d5e925a0acb91ca
ddozza54 <bananamayong@gmail.com>
2024-11-04 13:06:04 +0900

    feat: 로또 구입 금액 입력 받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+const { Console, Random } = MissionUtils;
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      const purchaseAmount = Number(
+        await Console.readLineAsync(
+          '구입 금액을 입력해 주세요.'
+        )
+      );
+      if (!purchaseAmount) {
+        throw Error('숫자로 입력해주세요');
+      }
+      if (purchaseAmount % 1000 !== 0) {
+        throw Error('1,000원 단위로 입력해주세요');
+      }
+      Console.print(purchaseAmount);
+    } catch (error) {
+      throw Error(`[ERROR] ${error.message}`);
+    }
+  }
 }
 
 export default App;
